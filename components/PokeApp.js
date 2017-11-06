0b5bfa7327b38a059a1ec718e62dd462ae5979e0
Alejandro Barazarte <alejandrobarazarte@hotmail.com>
2017-11-06 11:20:27 -0300

    Implemented socketio on clients

--- components/PokeApp.js
+++ components/PokeApp.js
@@ -5,6 +5,8 @@
 import uid from 'uid';
 import React from 'react';
 import $ from 'jquery';
+import io from 'socket.io-client';
+
 import PokeTable from './PokeTable';
 import PokeChat from './PokeChat';
 
@@ -17,19 +19,31 @@ export default class PokeApp extends React.Component {
       pokemons: []
     };
     this.onGrowl = this.onGrowl.bind(this);
+    this.user = uid(10);
   }
 
   componentWillMount() {
     $.get('/api/pokemons', (pokemons) => {
       this.setState({ pokemons: pokemons });
     });
+    this.socket = io('http://localhost:3000');
+    this.socket.on('message', (message) => {
+      if(message.user !== this.user){
+        this.newMessage(message);
+      }
+    });
   }
 
   onGrowl(name) {
     let text = `${name}, ${name}`;
+    let message = { id: uid(), text: text, user: this.user };
+    this.newMessage(message);
+    this.socket.emit('message', message); 
+  }
+
+  newMessage(message) {
+    this.state.messages.push(message);
     let messages = this.state.messages;
-    let message = { id: uid(), text: text };
-    messages.push(message);
     this.setState({ messages: messages });
   }
 
